cdab5083dac5682e5e2e7ec81e2c74572b710b8f
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-07 21:26:10 +0100

    small fixes

--- src/pages/Destinations.tsx
+++ src/pages/Destinations.tsx
@@ -21,7 +21,7 @@ const data = [
     alt: "planet mars",
     description:
       "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
-    distance: "225 mil. km",
+    distance: "225 million km",
     travel: "9 months",
   },
   {
@@ -30,7 +30,7 @@ const data = [
     alt: "titan, jupiters moon",
     description:
       "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
-    distance: "628 mil. km",
+    distance: "628 million km",
     travel: "3 years",
   },
   {
@@ -39,7 +39,7 @@ const data = [
     alt: "titan, saturns moon",
     description:
       "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
-    distance: "1.6 bil. km",
+    distance: "1.6 billion km",
     travel: "7 years",
   },
 ];
@@ -62,7 +62,9 @@ export default function Destinations() {
           <li
             key={index}
             className={`font-secondary text-white ${
-              index == currentTab && "border-b-[0.1875rem] border-b-white"
+              index == currentTab
+                ? "border-b-2 border-b-white"
+                : "hover:border-b-2 hover:border-b-white hover:border-opacity-50"
             }`}
           >
             <button
@@ -79,7 +81,7 @@ export default function Destinations() {
       <h3 className="font-primary text-6xl text-white uppercase">
         {data[currentTab].name}
       </h3>
-      <p className="font-secondary text-light-purple text-center mx-4 pb-8 mb-8 border-b border-b-light-purple">
+      <p className="font-secondary text-light-purple text-center mx-7 pb-8 mb-8 border-b border-b-light-purple">
         {data[currentTab].description}
       </p>
       <p className="uppercase tracking-widest font-secondary text-light-purple grid text-center mb-8">
@@ -89,7 +91,7 @@ export default function Destinations() {
         </span>
       </p>
       <p className="uppercase tracking-widest font-secondary text-light-purple grid text-center mb-16">
-        Travel time{" "}
+        Estimated travel time
         <span className="font-primary text-3xl text-white tracking-normal">
           {data[currentTab].travel}
         </span>
