4624f5e10baa80b69dc70df33afe3d3b6ba9a27c
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-08 15:48:42 +0100

    explore link and styling fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 
 export default function Home() {
   return (
     <MainLayout background="home">
-      <h2 className="grid gap-6 uppercase font-secondary text-base text-light-purple text-center tracking-widest">
+      <h2 className="grid gap-6 uppercase font-secondary text-base text-light-purple text-center tracking-widest mt-8">
         So you want to travel to{" "}
         <span className="font-primary text-7xl text-white">space</span>
       </h2>
@@ -13,11 +14,14 @@ export default function Home() {
         back, and relax because we’ll give you a truly out of this world
         experience!
       </p>
-      <button className="grid place-items-center  uppercase font-primary mb-8 before:absolute before:size-40 before:bg-white before:bg-opacity-10 before:rounded-full before:z-0 before:content-none hover:before:content-[''] hover:before:animate-pulse">
+      <Link
+        to="/destinations"
+        className="grid place-items-center  uppercase font-primary mb-8 before:absolute before:size-40 before:bg-white before:bg-opacity-10 before:rounded-full before:z-0 before:content-none hover:before:content-[''] hover:before:animate-pulse"
+      >
         <span className="bg-white size-32 rounded-full grid place-items-center z-0">
           explore
         </span>
-      </button>
+      </Link>
     </MainLayout>
   );
 }
