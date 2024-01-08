3ae0d6ce33cde2be4af2eeb9b90bc9f51357dafc
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-08 16:43:14 +0100

    styled tablet header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,13 @@ import logoUrl from "../assets/shared/logo.svg";
 import hamburgerUrl from "../assets/shared/icon-hamburger.svg";
 import closeUrl from "../assets/shared/icon-close.svg";
 import { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+interface LocationState {
+  from: {
+    pathname: string;
+  };
+}
 
 export default function Header() {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
@@ -31,13 +37,31 @@ export default function Header() {
     };
   }, []);
 
+  let location: string | number = useLocation().pathname;
+
+  switch (location) {
+    case "/destinations":
+      location = 1;
+      break;
+    case "/crew":
+      location = 2;
+      break;
+    case "/technology":
+      location = 3;
+      break;
+    default:
+      location = 0;
+  }
+
+  console.log(location);
+
   return (
     <header className="bg-lighter-black p-4 flex justify-between items-center">
       <Link to="/">
         <img
           src={logoUrl}
           alt="Space Tourism logo - home"
-          className="size-10 rounded-full"
+          className="size-10 rounded-full tablet:ml-6"
         />
       </Link>
 
@@ -47,7 +71,7 @@ export default function Header() {
           event.stopPropagation();
           setIsExpanded((value) => !value);
         }}
-        className="z-50"
+        className="z-50 tablet:hidden"
       >
         {isExpanded ? (
           <img src={closeUrl} alt="close menu"></img>
@@ -59,19 +83,43 @@ export default function Header() {
         ref={nav}
         className={`bg-opacity-5 bg-black backdrop-blur-2xl ${
           isExpanded ? "absolute" : "hidden"
-        } right-0 top-0 h-full w-full max-w-64 p-4 pt-20 z-40`}
+        } tablet:flex right-0 top-0 h-full w-full max-w-64 tablet:max-w-min p-4 pt-20 tablet:pt-4 z-40`}
       >
-        <ul className="flex flex-col gap-4 uppercase text-white font-secondary text-xl tracking-widest">
-          <li className="flex gap-2 before:content-['00'] before:font-bold py-2">
+        <ul className="flex flex-col tablet:flex-row gap-4 uppercase text-white font-secondary text-xl tracking-widest">
+          <li
+            className={`flex gap-2 before:content-['00'] before:font-bold tablet:before:content-none py-2 tablet:border-b-2 tablet:border-opacity-0 tablet:border-b-white ${
+              location == 0
+                ? "tablet:border-opacity-100"
+                : "tablet:hover:border-opacity-25"
+            }`}
+          >
             <Link to="/">Home</Link>
           </li>
-          <li className="flex gap-2 before:content-['01'] before:font-bold py-2">
+          <li
+            className={`flex gap-2 before:content-['00'] before:font-bold tablet:before:content-none py-2 tablet:border-b-2 tablet:border-opacity-0 tablet:border-b-white ${
+              location == 1
+                ? "tablet:border-opacity-100"
+                : "tablet:hover:border-opacity-25"
+            }`}
+          >
             <Link to="/destinations">Destination</Link>
           </li>
-          <li className="flex gap-2 before:content-['02'] before:font-bold py-2">
+          <li
+            className={`flex gap-2 before:content-['00'] before:font-bold tablet:before:content-none py-2 tablet:border-b-2 tablet:border-opacity-0 tablet:border-b-white ${
+              location == 2
+                ? "tablet:border-opacity-100"
+                : "tablet:hover:border-opacity-25"
+            }`}
+          >
             <Link to="/crew">Crew</Link>
           </li>
-          <li className="flex gap-2 before:content-['03'] before:font-bold py-2">
+          <li
+            className={`flex gap-2 before:content-['00'] before:font-bold tablet:before:content-none py-2 tablet:border-b-2 tablet:border-opacity-0 tablet:border-b-white ${
+              location == 3
+                ? "tablet:border-opacity-100"
+                : "tablet:hover:border-opacity-25"
+            }`}
+          >
             <Link to="/technology">Technology</Link>
           </li>
         </ul>
