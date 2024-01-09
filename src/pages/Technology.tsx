ec91a3115cc8528c6ae94cf7eac497b8e66fb85e
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-09 16:50:06 +0100

    technology desktop styling

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -36,42 +36,45 @@ export default function Technology() {
 
   return (
     <MainLayout background="technology">
-      <h2 className="flex gap-4 uppercase font-secondary text-white before:content-['03'] before:opacity-25 tracking-widest mt-8 mb-8">
-        Space launch 101
-      </h2>
-      <picture>
-        <source
-          media="(min-width: 64rem)"
-          srcSet={`${data[currentTab].images.portrait}`}
-        />
-        <img src={data[currentTab].images.landscape} alt="" className="mb-8" />
-      </picture>
-
-      <div role="tablist" className="flex flex-wrap gap-4 mx-4 mb-4">
-        {data.map((_, index) => (
-          <button
-            key={index}
-            role="tab"
-            aria-selected={index == currentTab}
-            onClick={() => setCurrentTab(index)}
-            className={`tracking-widest uppercase font-secondary text-white p-4 border border-white rounded-full size-10 flex justify-center items-center border-opacity-25 ${
-              index == currentTab
-                ? "bg-white !text-black"
-                : "hover:border-opacity-100"
-            }`}
-          >
-            {index + 1}
-          </button>
-        ))}
+      <div className="grid place-items-center desktop:grid-cols-[auto_1fr_auto] w-full desktop:pl-36 gap-x-12 gap-y-4 ">
+        <h2 className="flex gap-4 uppercase font-secondary text-white before:content-['03'] before:opacity-25 tracking-widest mt-8 mb-8 desktop:text-3xl desktop:col-[1/4] desktop:w-full">
+          Space launch 101
+        </h2>
+        <picture className="mb-8 desktop:mb-0 desktop:row-[2/4] desktop:col-[3/4]">
+          <source
+            media="(min-width: 64rem)"
+            srcSet={`${data[currentTab].images.portrait}`}
+          />
+          <img src={data[currentTab].images.landscape} alt="" />
+        </picture>
+        <div
+          role="tablist"
+          className="flex flex-wrap gap-4 mx-4 mb-4 desktop:row-[2/4] desktop:flex-col"
+        >
+          {data.map((_, index) => (
+            <button
+              key={index}
+              role="tab"
+              aria-selected={index == currentTab}
+              onClick={() => setCurrentTab(index)}
+              className={`tracking-widest uppercase font-secondary text-white p-4 border border-white rounded-full size-10 flex justify-center items-center border-opacity-25 desktop:size-20 ${
+                index == currentTab
+                  ? "bg-white !text-black"
+                  : "hover:border-opacity-100"
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+        <h3 className="uppercase font-primary text-center text-light-purple grid mt-4 desktop:text-left desktop:w-full desktop:[align-self:end]">
+          The terminology...
+          <span className="text-3xl text-white">{data[currentTab].name}</span>
+        </h3>
+        <p className="font-secondary text-center text-light-purple mx-4 mb-16 mt-4 tablet:max-w-[28rem] leading-7 desktop:text-left desktop:max-w-none desktop:[align-self:start] desktop:ml-0">
+          {data[currentTab].description}
+        </p>
       </div>
-
-      <h3 className="uppercase font-primary text-center text-light-purple grid mt-4">
-        The terminology...
-        <span className="text-3xl text-white">{data[currentTab].name}</span>
-      </h3>
-      <p className="font-secondary text-center text-light-purple mx-4 mb-16 mt-4 tablet:max-w-[28rem] leading-7">
-        {data[currentTab].description}
-      </p>
     </MainLayout>
   );
 }
